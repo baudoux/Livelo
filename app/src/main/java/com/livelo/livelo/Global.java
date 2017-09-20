17b9d9f2278e1171405507f0f575b98001b5e104
Remi <remi.baudoux@epfl.ch>
2017-09-20 17:31:14 +0200

    commit 2

--- app/src/main/java/com/livelo/livelo/Global.java
+++ app/src/main/java/com/livelo/livelo/Global.java
@@ -15,25 +15,5 @@ public class Global {
     public static int[] myarray1=new int[10];
     public static Sensor tmp_sensor;
 
-    public boolean fileExists(String name){
-        try {
-            FileInputStream fileIn = openFileInput(name);
-            InputStreamReader InputRead = new InputStreamReader(fileIn);
-            int c = InputRead.read();
-
-            if(c == -1) {
-                InputRead.close();
-                return false;
-            }
-            else{
-                InputRead.close();
-                return true;
-            }
-
-        }catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 }
 
